0e73899299f54beddb571e72e485e3a0e42b2865
Bhawna Rajput <bhawnarajput140700@gmail.com>
2022-03-07 17:54:33 +0530

    Prepare for deployment

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://guptanamannn:Feb@dell2022@cluster0.5tmp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
