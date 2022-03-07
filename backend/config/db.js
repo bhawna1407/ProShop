a6461c8b3f10a2fe40e026d534d616787a7cec85
Bhawna Rajput <bhawnarajput140700@gmail.com>
2022-03-07 22:01:51 +0530

    my project

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://guptanamannn:Feb@dell2022@cluster0.5tmp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    const conn = await mongoose.connect('mongodb+srv://bhavana:Feb@dell2022@cluster0.5tmp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
